9d8f853e217f1943a3b69feba77b04130bdcddfa
MahesaYogha <116375160+MahesaYogha@users.noreply.github.com>
2023-10-30 18:25:56 +0700

    latihan2.php

--- latihan2.php
+++ latihan2.php
@@ -1,22 +1,22 @@
 <!DOCTYPE html>
 <html>
-    <body>
-	<h1>Selamat Datang</h1>
-	<br>
-        <div class="form-floating mb-3">
-            <input type="email" class="form-control" id="floatingInput" placeholder="Input angka">
-            
-          </div>
-          <br>
-          <select class="form-select" aria-label="Default select example">
-            <option selected>tekan untuk melihat lebih banyak</option>
-            <option value="1">One</option>
-            <option value="2">Two</option>
-            <option value="3">Three</option>
-          </select>
-          <br>
-          <br>
+<head>
+    <title>latihan2</title>
+</head>
+<body>
+    <h1>LATIHAN 2 WAD</h1>
+    
+    <form method="post" action="latihan3.php">
+        <label for="InputAngka1">Masukkan angka: </label>
+        <input type="number" id="InputAngka1" name="inputangka" required><br><br>
+	
+        <label for="Dropdown">Pengoperasian: </label>
+        <select id="Dropdown" name="Dropdown">
+            <option value="pilihan1">+ 10</option>
+            <option value="pilihan2">* 5</option>
+            <option value="pilihan3">- 30</option>
+        </select><br><br>
 
-        <button type="button" class="btn">Hitung</button>
-    </body>
+        <button type="submit" name="hitung">Hitung</button>
+    </form>
 </html>
